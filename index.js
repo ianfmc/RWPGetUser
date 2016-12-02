90ad4083f9309103115a3467788f5f6f5ad86910
Ian McCallum <ianfmc@gmail.com>
2016-12-02 13:52:56 -0800

    Fix get call

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ exports.handler = function(event, context, callback) {
     var userEmail = event.email;
 
     var docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
-    var teamParams = {
-        TableName : 'user',
+    var userParams = {
+        TableName : 'User',
         Key : {
             email: userEmail
         },
     }
-    docClient.get(teamParams, function(err, data) {
+    docClient.get(userParams, function(err, data) {
         if (err) {
-            callback(new Error('Unknown Team'));
+            callback(new Error('Unknown User'));
         }
         else {
             callback(null, data);
