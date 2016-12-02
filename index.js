d12720dfb03ef1181bb5401c1066fc14d881f05e
Ian McCallum <ianfmc@gmail.com>
2016-12-02 14:23:43 -0800

    Add console log for debugging

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var AWS = require('aws-sdk');
 
 exports.handler = function(event, context, callback) {
     
+    var docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
     var userEmail = event.email.toString();
 
-    var docClient = new AWS.DynamoDB.DocumentClient({region: 'us-east-1'});
+    console.log(userEmail);
+
     var userParams = {
         TableName : 'User',
         Key : {
